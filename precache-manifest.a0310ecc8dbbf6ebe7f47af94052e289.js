f2017270d19d42d4333650b7834f5d6163a189ce
Lisa Pena <lisapena13@gmail.com>
2020-09-21 11:47:10 -0500

    Updates

---
+++ precache-manifest.a0310ecc8dbbf6ebe7f47af94052e289.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfe931d90f6df3f7781473aad0650439",
+    "url": "/minesweeper/index.html"
+  },
+  {
+    "revision": "800bf95d15653b36b555",
+    "url": "/minesweeper/static/css/main.1276d2ea.chunk.css"
+  },
+  {
+    "revision": "a4e88b40815776e6486f",
+    "url": "/minesweeper/static/js/2.2412ca34.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/minesweeper/static/js/2.2412ca34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "800bf95d15653b36b555",
+    "url": "/minesweeper/static/js/main.f1ee343b.chunk.js"
+  },
+  {
+    "revision": "03fdde6e651e9bdac82d",
+    "url": "/minesweeper/static/js/runtime-main.9c76a611.js"
+  }
+]);
